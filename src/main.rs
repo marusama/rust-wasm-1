8af977abbb038afbe8367ad259968f65dc0bbed9
marusama <maru.sama@gmail.com>
2020-04-29 01:18:58 +0300

    main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
+use std::{env, fs};
+
+fn process(current_dir: &str) -> Result<(), String> {
+    for entry in fs::read_dir(current_dir).map_err(|err| format!("{}", err))? {
+        let entry = entry.map_err(|err| format!("{}", err))?;
+        let path = entry.path();
+        let metadata = fs::metadata(&path).map_err(|err| format!("{}", err))?;
+        println!(
+            "filename: {:?}, filesize: {:?} bytes",
+            path.file_name()
+                .ok_or("No filename")
+                .map_err(|err| err.to_string())?,
+            metadata.len()
+        );
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
-
-    let a = 1;
-
-    println!("{}", a)
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    if args.len() < 2 {
+        eprintln!("{} <input_folder>", program);
+        return;
+    }
+    if let Err(err) = process(&args[1]) {
+        eprintln!("{}", err)
+    }
 }
